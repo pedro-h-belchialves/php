549e7d7aeb80a8899821e7b131475e99829ab277
PEDRO HENRIQUE BELCHIOR ALVES <125604076+pedro-h-belchialves@users.noreply.github.com>
2025-04-13 22:33:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+echo "<h1>🚀 Deploy no Azure com PHP funcionando!</h1>";
+echo "<p>Olá, hoje é " . date("d/m/Y") . " e agora são " . date("H:i:s") . "</p>";
+echo "<p>Parabéns! Seu código está rodando na nuvem ✨</p>";
+?>
